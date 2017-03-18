3c3d6513e811d1b932eb70a9ceb6c6df76f91156
Diakiv Rostyslav <rostik0945@gmail.com>
2017-03-18 21:36:09 +0200

    db

---
+++ Coma/Coma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = { 1, 2, 3, 4, 5, 56, 6, 7, 8, 98 };
+            Console.Write(mas[0]);
+            for(int i = 1; i < mas.Length; i++)
+            {
+                Console.Write(", " + mas[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
